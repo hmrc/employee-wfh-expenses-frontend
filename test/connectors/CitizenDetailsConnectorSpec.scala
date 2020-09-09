ce8636e0d05c90caaa8740f7da85d2307547def8
danny-waughman <63302027+danny-waughman@users.noreply.github.com>
2020-09-09 16:47:46 +0100

    DL-4138 Adding ability to fetch users ETag

--- test/connectors/CitizenDetailsConnectorSpec.scala
+++ test/connectors/CitizenDetailsConnectorSpec.scala
@@ -22,7 +22,7 @@ import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatestplus.mockito.MockitoSugar
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.Application
-import play.api.http.Status.OK
+import play.api.http.Status._
 import play.api.inject.guice.GuiceApplicationBuilder
 import uk.gov.hmrc.http.HttpResponse
 import utils.WireMockHelper
@@ -61,6 +61,148 @@ class CitizenDetailsConnectorSpec extends SpecBase with MockitoSugar with WireMo
           res.body mustBe validAddressJson.toString
       }
 
+    }
+
+    "return a 400 on failure" in {
+      server.stubFor(
+        get(urlEqualTo(s"/citizen-details/$fakeNino/designatory-details"))
+          .willReturn(
+            aResponse()
+              .withStatus(BAD_REQUEST)
+              .withBody(validAddressJson.toString)
+          )
+      )
+
+      val result = citizenDetailsConnector.getAddress(fakeNino)
+
+      whenReady(result) {
+        res =>
+          res mustBe a[HttpResponse]
+          res.status mustBe 400
+          res.body mustBe validAddressJson.toString
+      }
+
+    }
+
+    "return a 404 on failure" in {
+      server.stubFor(
+        get(urlEqualTo(s"/citizen-details/$fakeNino/designatory-details"))
+          .willReturn(
+            aResponse()
+              .withStatus(NOT_FOUND)
+              .withBody(validAddressJson.toString)
+          )
+      )
+
+      val result = citizenDetailsConnector.getAddress(fakeNino)
+
+      whenReady(result) {
+        res =>
+          res mustBe a[HttpResponse]
+          res.status mustBe 404
+          res.body mustBe validAddressJson.toString
+      }
+
+    }
+
+    "return a 500 on failure" in {
+      server.stubFor(
+        get(urlEqualTo(s"/citizen-details/$fakeNino/designatory-details"))
+          .willReturn(
+            aResponse()
+              .withStatus(INTERNAL_SERVER_ERROR)
+              .withBody(validAddressJson.toString)
+          )
+      )
+
+      val result = citizenDetailsConnector.getAddress(fakeNino)
+
+      whenReady(result) {
+        res =>
+          res mustBe a[HttpResponse]
+          res.status mustBe 500
+          res.body mustBe validAddressJson.toString
+      }
+
+    }
+  }
+
+  "getETag" must {
+    "return an etag on success" in {
+      server.stubFor(
+        get(urlEqualTo(s"/citizen-details/$fakeNino/etag"))
+          .willReturn(
+            aResponse()
+              .withStatus(OK)
+              .withBody(validETagJson.toString)
+          )
+      )
+
+      val result = citizenDetailsConnector.getETag(fakeNino)
+
+      whenReady(result) {
+        res =>
+          res mustBe a[HttpResponse]
+          res.status mustBe 200
+          res.body mustBe validETagJson.toString
+      }
+    }
+
+    "return 500 on failure" in {
+      server.stubFor(
+        get(urlEqualTo(s"/citizen-details/$fakeNino/etag"))
+          .willReturn(
+            aResponse()
+              .withStatus(INTERNAL_SERVER_ERROR)
+          )
+      )
+
+      val result: Future[HttpResponse] = citizenDetailsConnector.getETag(fakeNino)
+
+      whenReady(result) {
+        res =>
+          res mustBe a[HttpResponse]
+          res.status mustBe 500
+          res.body mustBe ""
+      }
+    }
+
+    "return 401 on failure" in {
+      server.stubFor(
+        get(urlEqualTo(s"/citizen-details/$fakeNino/etag"))
+          .willReturn(
+            aResponse()
+              .withStatus(UNAUTHORIZED)
+          )
+      )
+
+      val result: Future[HttpResponse] = citizenDetailsConnector.getETag(fakeNino)
+
+      whenReady(result) {
+        res =>
+          res mustBe a[HttpResponse]
+          res.status mustBe 401
+          res.body mustBe ""
+      }
+    }
+
+    "return 404 on failure" in {
+      server.stubFor(
+        get(urlEqualTo(s"/citizen-details/$fakeNino/etag"))
+          .willReturn(
+            aResponse()
+              .withStatus(NOT_FOUND)
+          )
+      )
+
+      val result: Future[HttpResponse] = citizenDetailsConnector.getETag(fakeNino)
+
+      whenReady(result) {
+        res =>
+          res mustBe a[HttpResponse]
+          res.status mustBe 404
+          res.body mustBe ""
+      }
     }
   }
 

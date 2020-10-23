8aaab4a91aaef9ce52ee9ce8e5c9e40020680c69
grahampaulcook <grahampaulcook@users.noreply.github.com>
2020-10-23 09:19:23 +0100

    DL-4460 Tech error page to be 500 not 200 http response

--- app/controllers/TechnicalDifficultiesController.scala
+++ app/controllers/TechnicalDifficultiesController.scala
@@ -20,7 +20,7 @@ import javax.inject.Inject
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
-import views.html.{ErrorTemplate, TechnicalErrorView}
+import views.html.TechnicalErrorView
 
 class TechnicalDifficultiesController @Inject()(
                                                  val controllerComponents: MessagesControllerComponents,
@@ -28,6 +28,6 @@ class TechnicalDifficultiesController @Inject()(
                                                ) extends FrontendBaseController with I18nSupport {
 
   def onPageLoad: Action[AnyContent] = Action { implicit request =>
-    Ok(technicalErrorView())
+    InternalServerError(technicalErrorView())
   }
 }

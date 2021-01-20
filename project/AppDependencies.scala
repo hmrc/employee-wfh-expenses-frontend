b926c6389996c420d264a5c6164be5c5037751c7
grahampaulcook <grahampaulcook@users.noreply.github.com>
2021-01-20 15:40:50 +0000

    DL-4866 Update to Play 2.7

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,42 +1,28 @@
 import sbt._
 
 object AppDependencies {
-  import play.core.PlayVersion
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "org.reactivemongo" %% "play2-reactivemongo"            % "0.18.6-play26",
-    "uk.gov.hmrc"       %% "logback-json-logger"            % "4.8.0",
-    "uk.gov.hmrc"       %% "play-health"                    % "3.15.0-play-26",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.3.0-play-26",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-26"     % "3.0.0",
-    "uk.gov.hmrc"       %% "play-frontend-govuk"            % "0.53.0-play-26",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "0.22.0-play-26",
-    "uk.gov.hmrc"       %% "play-partials"                  % "7.0.0-play-26",
+    "org.reactivemongo" %% "play2-reactivemongo"            % "0.20.2-play27",
+    "uk.gov.hmrc"       %% "logback-json-logger"            % "4.9.0",
+    "uk.gov.hmrc"       %% "play-health"                    % "3.16.0-play-27",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.5.0-play-27",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-27"     % "3.3.0",
+    "uk.gov.hmrc"       %% "play-frontend-govuk"            % "0.60.0-play-27",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "0.37.0-play-27",
+    "uk.gov.hmrc"       %% "play-partials"                  % "7.1.0-play-27",
     "uk.gov.hmrc"       %% "tax-year"                       % "1.2.0"
   )
 
   val test = Seq(
-    "org.scalatest"               %% "scalatest"            % "3.0.7",
-    "org.scalatestplus.play"      %% "scalatestplus-play"   % "3.1.2",
-    "org.pegdown"                  %  "pegdown"             % "1.6.0",
-    "org.jsoup"                    %  "jsoup"               % "1.10.3",
-    "com.typesafe.play"           %% "play-test"            % PlayVersion.current,
-    "org.mockito"                  %  "mockito-all"         % "1.10.19",
+    "org.scalatestplus.play"      %% "scalatestplus-play"   % "4.0.3",
+    "org.pegdown"                  % "pegdown"              % "1.6.0",
+    "org.jsoup"                    % "jsoup"                % "1.13.1",
+    "org.mockito"                  % "mockito-all"          % "1.10.19",
     "org.scalacheck"              %% "scalacheck"           % "1.14.0",
-    "com.github.tomakehurst"       %  "wiremock-standalone" % "2.26.3"
+    "com.github.tomakehurst"       % "wiremock-standalone"  % "2.26.3"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
-
-  val akkaVersion = "2.5.23"
-  val akkaHttpVersion = "10.0.15"
-
-  val overrides = Seq(
-    "com.typesafe.akka" %% "akka-stream" % akkaVersion,
-    "com.typesafe.akka" %% "akka-protobuf" % akkaVersion,
-    "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
-    "com.typesafe.akka" %% "akka-actor" % akkaVersion,
-    "com.typesafe.akka" %% "akka-http-core" % akkaHttpVersion
-  )
 }

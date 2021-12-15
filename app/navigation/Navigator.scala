d2a5d48437274710d9346d8a63ddd343f1a48c8e
Pankaj Khare <12529426+pankaj2453@users.noreply.github.com>
2021-12-15 09:17:45 +0000

    DL-6902 change navigation for start-date page

--- app/navigation/Navigator.scala
+++ app/navigation/Navigator.scala
@@ -30,13 +30,22 @@ class Navigator @Inject()() extends Logging {
 
   private val normalRoutes: Page => UserAnswers => Call = {
     case ClaimedForTaxYear2020 => ua => claimJourneyFlow(ua)
-    case SelectTaxYearsToClaimForPage => _ => routes.DisclaimerController.onPageLoad()
+    case SelectTaxYearsToClaimForPage => ua => startDateJourneyFlow(ua)
+    //case SelectTaxYearsToClaimForPage => _ => routes.DisclaimerController.onPageLoad()
     case DisclaimerPage => ua => disclaimerNextPage(ua)
     case WhenDidYouFirstStartWorkingFromHomePage => ua => checkStartWorkingFromHomeDate(ua)
     case _ => _ => routes.IndexController.onPageLoad()
   }
 
   def nextPage(page: Page, userAnswers: UserAnswers): Call = normalRoutes(page)(userAnswers)
+
+  def startDateJourneyFlow(userAnswers: UserAnswers): Call = {
+    if (userAnswers.get(SelectTaxYearsToClaimForPage).get.contains(SelectTaxYearsToClaimFor.Option2)) {
+      routes.WhenDidYouFirstStartWorkingFromHomeController.onPageLoad()
+    } else {
+      routes.DisclaimerController.onPageLoad()
+    }
+  }
 
   def checkStartWorkingFromHomeDate(userAnswers: UserAnswers): Call = {
     val earliestStartDate = LocalDate.of(2020,1,1)

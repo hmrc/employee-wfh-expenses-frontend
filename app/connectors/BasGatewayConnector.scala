6ed8437c4206f12a6a1f35d927a210ddd1d6f092
matjoseph24 <mathew.a.joseph@accenture.com>
2025-05-19 15:24:15 +0100

    DL-16365: Addressed Jenkins scalafmt error

--- app/connectors/BasGatewayConnector.scala
+++ app/connectors/BasGatewayConnector.scala
@@ -16,35 +16,32 @@
 
 package connectors
 
-  import play.api.Logging
-  import sttp.model.HeaderNames.ContentType
-  import sttp.model.MediaType.ApplicationJson
-  import config.FrontendAppConfig
-  import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse, StringContextOps}
-  import uk.gov.hmrc.http.client.HttpClientV2
+import play.api.Logging
+import sttp.model.HeaderNames.ContentType
+import sttp.model.MediaType.ApplicationJson
+import config.FrontendAppConfig
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse, StringContextOps}
+import uk.gov.hmrc.http.client.HttpClientV2
 
-  import javax.inject.Inject
-  import scala.concurrent.{ExecutionContext, Future}
-  import scala.util.Failure
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Failure
 
-  class BasGatewayConnector @Inject() (http: HttpClientV2, appConfig: FrontendAppConfig) extends Logging {
+class BasGatewayConnector @Inject() (http: HttpClientV2, appConfig: FrontendAppConfig) extends Logging {
 
-    private val EmptyJsonStr = "{}"
+  private val EmptyJsonStr = "{}"
 
-    def signOutUser()(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[HttpResponse] = {
-      val basGatewayFrontendBaseUrl = appConfig.basGatewayBaseUrl
-      val signOutPath               = "/bas-gateway/logout-without-state"
-      val fullSignOutUrl            = s"$basGatewayFrontendBaseUrl$signOutPath"
+  def signOutUser()(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[HttpResponse] = {
+    val basGatewayFrontendBaseUrl = appConfig.basGatewayBaseUrl
+    val signOutPath               = "/bas-gateway/logout-without-state"
+    val fullSignOutUrl            = s"$basGatewayFrontendBaseUrl$signOutPath"
 
-      http
-        .post(url"$fullSignOutUrl")
-        .setHeader(ContentType -> ApplicationJson.toString())
-        .withBody(EmptyJsonStr)
-        .execute[HttpResponse]
-        .andThen { case Failure(error) => logger.warn(s"Received error response from bas-gateway: ${error.getMessage}") }
-    }
-
-
-
+    http
+      .post(url"$fullSignOutUrl")
+      .setHeader(ContentType -> ApplicationJson.toString())
+      .withBody(EmptyJsonStr)
+      .execute[HttpResponse]
+      .andThen { case Failure(error) => logger.warn(s"Received error response from bas-gateway: ${error.getMessage}") }
+  }
 
 }

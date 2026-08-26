1087e6f283e2d1b2ce2e8ae4889803b6e036f01d
eloiseyaz <79723491+eloiseyaz@users.noreply.github.com>
2026-08-26 12:17:38 +0100

    update scaWrapperPlayVersion

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  val scaWrapperPlayVersion = "6.1.0"
+  val scaWrapperPlayVersion = "6.2.0"
   val mongoPlayVersion      = "2.13.0"
 
   val compile = Seq(
